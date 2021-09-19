d2d9bbd7d94f9b48ca4064335e297ff4a393d8ab
Marija Budinoska <marijabudinoska@yahoo.com>
2021-09-19 17:58:17 +0200

    Init commit

---
+++ src/test/java/code/academy/peopleapi/InitialTestFile.java
@@ -0,0 +1,27 @@
+package code.academy.peopleapi;
+
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.Test;
+
+import javax.net.ssl.SSLContext;
+
+
+public class InitialTestFile {
+    @Test
+    public void InitialTest() throws Exception {
+       
+
+    }
+}
