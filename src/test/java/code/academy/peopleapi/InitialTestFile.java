f9fb2e2fe1a5e015601da55f5cfbc18647792644
Marija Budinoska <marijabudinoska@yahoo.com>
2021-09-19 17:59:52 +0200

    Add automation day 1 commit

--- src/test/java/code/academy/peopleapi/InitialTestFile.java
+++ src/test/java/code/academy/peopleapi/InitialTestFile.java
@@ -21,7 +21,24 @@ import javax.net.ssl.SSLContext;
 public class InitialTestFile {
     @Test
     public void InitialTest() throws Exception {
-       
+        Header contentType = new BasicHeader(HttpHeaders.CONTENT_TYPE,"application/json");
 
+        SSLContext sslContext = SSLContextBuilder
+                .create()
+                .loadTrustMaterial(new TrustSelfSignedStrategy())
+                .build();
+
+        HttpGet request = new HttpGet("https://people-api1.herokuapp.com/");
+        request.setHeader(contentType);
+
+        HttpClient httpClient = HttpClients.custom().setSSLContext(sslContext).build();
+
+        HttpResponse response = httpClient.execute(request);
+
+        HttpEntity entity = response.getEntity();
+        String body = EntityUtils.toString(response.getEntity());
+
+        HttpEntity newEntity = new StringEntity(body, ContentType.get(entity));
+        response.setEntity(newEntity);
     }
 }
